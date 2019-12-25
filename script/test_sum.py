551bebd0329e19a941716402aabc24ae94b067ba
HCF <18830381357@163.com>
2019-12-25 18:24:11 +0800

    pytest.ini update

--- script/test_sum.py
+++ script/test_sum.py
@@ -4,11 +4,12 @@
 import os
 import sys
 
+import allure
+
 sys.path.append(os.getcwd())
 import pytest
 
 from base.get_data import GetData
-
 
 
 def data_01():
@@ -25,10 +26,15 @@ def data_01():
 
 
 class TestSum:
+    # Serverity的严重级别（）
+    @allure.step("我是步骤的描述，第一步……第二步……")
+    @pytest.allure.severity(pytest.allure.severity_level.NORMAL)
     @pytest.mark.parametrize("a, b, c", data_01())
     def test_sum(selfa, a, b, c):
         """
         判断两个数的和是否和预期的一致
         :return:
         """
+        # 添加步骤描述信息----allure报告--和step搭配使用，对step的进一步说明
+        allure.attach("文件名字", "文件的具体内容，具体步骤的描述信息")
         assert a + b == c
